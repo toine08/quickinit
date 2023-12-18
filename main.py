c101c0bbbe1157b81a486be5a62ed112f694b5af
toto <50374594+toine08@users.noreply.github.com>
2023-12-18 12:57:49 +0000

    fix: refactorisation

--- main.py
+++ main.py
@@ -15,13 +15,17 @@ questions = [
 ]
 
 
-def create_python_project_structure(project_name):
+def create_python_project_structure():
+    project_name = input("Enter the project name:")
     src = 'templates/template_python'
     dest = os.path.join(os.getcwd(), project_name)
-
-    shutil.copytree(src,dest)
-    #os.rename(temp_path,project_name)
-
+    try:
+        shutil.copytree(src,dest)
+    except:
+        sys.exit("An error occured...")
+    finally:
+        end_program(project_name)
+    
 def create_go_project_structure():
     project_name = input("Enter the project name:")
     git_url = 'https://github.com/golang-standards/project-layout'
@@ -33,15 +37,17 @@ def create_go_project_structure():
     finally:
         end_program(project_name)
 
-
 def create_vanilla_project_structure():
     project_name = input("Enter the project name:")
     src = 'templates/template_web'
     dest = os.path.join(os.getcwd(), project_name)
-
-    shutil.copytree(src,dest)
-    #os.rename(temp_path,project_name)
-
+    try:
+        shutil.copytree(src,dest)
+    except:
+        sys.exit("An error occured...")
+    finally:
+        end_program(project_name)
+   
 def create_nextjs_project_structure():
     project_name = input("Enter the project name:")
     # Create basic Next.js project structure (customize as needed)
@@ -49,6 +55,8 @@ def create_nextjs_project_structure():
         subprocess.run(f'npx create-next-app@latest {project_name}', shell=True)
     except subprocess.CalledProcessError:
         sys.exit("Command not found please install npm or your favorite package management ")
+    finally:
+        end_program(project_name)
 
 def create_rust_project_structure():
     project_name = input("Enter the project name:")
@@ -56,6 +64,8 @@ def create_rust_project_structure():
         subprocess.run(f'cargo new {project_name}', shell=True)
     except subprocess.CalledProcessError:
         sys.exit("Command not found please install rust & cargo to process ")
+    finally:
+        end_program(project_name)
 
 def open_vscode(project_path):
     try:
@@ -76,20 +86,18 @@ def main():
     try:
         match project_type:
             case "NextJS":
-                #ADD project name func
-                create_nextjs_project_structure(input("Project name: "))
+                create_nextjs_project_structure()
                 end_program()
             case "Vanilla":
-                create_vanilla_project_structure(input("Project name: "))
+                create_vanilla_project_structure()
                 end_program()
             case "Golang":
-                create_go_project_structure(input("Project name: "))
+                create_go_project_structure()
                 end_program()
             case "Python":
-                create_python_project_structure(input("Project name: "))
-                end_program()
+                create_python_project_structure()
             case "Rust":
-                create_rust_project_structure(input("Project name: "))
+                create_rust_project_structure()
                 end_program()
             case "More":
                 print(f"If there is not the language you want to use you can write an issue on this github: https://github.com/toine08/clitool")
